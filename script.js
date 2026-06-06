329057e30cbc9d8e59d8dfd1d7a8deb181899df4
Tapan Rathore <tapanrathore07@gmail.com>
2026-06-06 22:48:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 //your JS code here. If required.
+function transformingData(){
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            const number = Number(document.getElementById("ip").value);
+            resolve(number)
+        }, 2000)
+    })
+    .then((number)=>{
+        return new Promise((resolve)=>{
+            setTimeout(()=>{
+                const multiNum = number*2;
+                document.getElementById("output").innerText="Result: "+ multiNum;
+                resolve(multiNum);
+            },2000)
+        })
+    })
+    .then((multiNum)=>{
+        return new Promise((resolve)=>{
+            setTimeout(()=>{
+                const subNum = multiNum-3;
+                document.getElementById("output").innerText="Result: "+ subNum;
+                resolve(subNum)
+            }, 1000)
+        })
+    })
+    .then((subNum)=>{
+        return new Promise((resolve)=>{
+            setTimeout(()=>{
+                const divNum = subNum/2;
+                document.getElementById("output").innerText="Result: "+ divNum;
+                resolve(divNum);
+            }, 1000)
+        })
+    })
+    .then((divNum)=>{
+        return new Promise((resolve)=>{
+            setTimeout(()=>{
+                const finalNum = divNum+10;
+                document.getElementById("output").innerText="Result: "+ finalNum;
+                resolve(finalNum)
+            },1000)
+        })
+    })
+}
